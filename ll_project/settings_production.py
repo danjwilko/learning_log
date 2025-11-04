4f136be5d86a08e76648e8b08a19ba579badaeb3
danjwilko <danielwilkinson0991@gmail.com>
2025-11-04 07:53:33 +0000

    Upadted the landing page to show the Topics page - impoved the flow.

--- ll_project/settings_production.py
+++ ll_project/settings_production.py
@@ -101,7 +101,7 @@ STATIC_ROOT = '/home/danjwilko/learning_log/static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings.
-LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGIN_REDIRECT_URL = 'learning_logs:topics'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
 LOGIN_URL = 'accounts:login'
 
